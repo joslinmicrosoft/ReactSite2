ff70d03c1e18554d25372daf682ad4e1cca73987
Joseph Lin <joslin@microsoft.com>
2020-02-03 11:18:59 -0800

    Update auth to this website

--- app/src/App.js
+++ app/src/App.js
@@ -9,37 +9,6 @@ import {
   Switch
 } from 'react-router-dom'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Look at this really cool spinny thing we have here.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Everything is fine.
-//         </a>
-//         <a 
-//           className="App-link"
-//           href="https://authentication.mitalikarmarkar.net/.auth/login/facebook?post_login_redirect_uri=serene-evergreen-1262eeae.mitalikarmarkar.net"
-//         >
-//         Log in with Facebook - Mitali change - see the build system works - 11/8 10:12am!
-//         </a>
-
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// const Home = () => (<div><h1>Welcome home</h1></div>)
 const Home = () => (<div><h1>Welcome MITALI</h1><Link to='/about'>Go to about</Link></div>)
 const About = ({ name }) => (<div><h1>About {name}</h1></div>)
 
@@ -54,9 +23,9 @@ class App extends React.Component {
               <div>
                 Last update made on 1/30/2020 1:27pm
                 <Home {...this.props} {...renderProps} />
-                <a href="https://gray-sky-10e8573e.mitalikarmarkar.net/.auth/login/facebook">Login and return to this page</a>
-                <a href="https://gray-sky-10e8573e.mitalikarmarkar.net/.auth/login/facebook?post_login_redirect_uri=gray-sky-10e8573e.mitalikarmarkar.net/authenticated">Authenticated page</a>
-                <a href="https://gray-sky-10e8573e.mitalikarmarkar.net/.auth/login/facebook?post_login_redirect_uri=gray-sky-10e8573e.mitalikarmarkar.net/admin">Admin page</a>
+                <a href="https://victorious-pebble-09d07414.staticsitescanary.net/.auth/login/facebook">Login and return to this page</a>
+                <a href="https://victorious-pebble-09d07414.staticsitescanary.net/.auth/login/facebook?post_login_redirect_uri=victorious-pebble-09d07414.staticsitescanary.net/authenticated">Authenticated page</a>
+                <a href="https://victorious-pebble-09d07414.staticsitescanary.net/.auth/login/facebook?post_login_redirect_uri=victorious-pebble-09d07414.staticsitescanary.net/admin">Admin page</a>
               </div>
             )} />
         </Switch>
